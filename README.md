b6a4e0beb0b73bb7d4675a73c1c6bb91c6785d08
Néstor Andrés <nestor2509@gmail.com>
2019-07-11 13:33:01 -0500

    script python para descargar las imagenes

--- README.md
+++ README.md
@@ -14,5 +14,26 @@ script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";
 
 // añadir el script al <head> del documento
 document.getElementsByTagName('head')[0].appendChild(script);
+
+// usando jquery se seleccionan los elementos que contienen las urls de las imagenes y se añaden a un arreglo
+var urls = $('.rg_di .rg_meta').map(function() { return JSON.parse($(this).text()).ou; });
+
+// se convierten los enlaces en el arreglo a un string con saltos de linea entre cada elemento
+var textToSave = urls.toArray().join('\n');
+
+// se crea un nuevo elemento <a> para descargar el archivo de texto
+var elementoDescarga = document.createElement('a');
+
+// se cambia el link del nuevo elemento [href] para que descargue un documento de texto
+elementoDescarga.href = 'data:attachment/text,' + encodeURI(textToSave);
+
+// se cambia el destino del elemento a para que cargue en una nueva ventana
+elementoDescarga.target = '_blank';
+
+// se pone un nombre al archivo
+elementoDescarga.download = 'urls.txt';
+
+// click y descarga del nuevo documento
+elementoDescarga.click();
 ```
 
---
+++ descarga-imagenes.py
@@ -0,0 +1,70 @@
+# importar las librerias necesarias
+from imutils import paths
+import argparse
+import requests
+import cv2
+import os
+
+# como el script se ejecuta desde la consola, se crean dos argumentos
+ap = argparse.ArgumentParser()
+
+# --urls es la ruta del archivo urls.txt que se creó desde la consola del navegador
+ap.add_argument("-u", "--urls", required=True,
+	help="ruta al archivo que contiene las URLs")
+
+# --output es la ruta de la carpeta en donde se van a descargar las imágenes
+ap.add_argument("-o", "--output", required=True,
+	help="ruta de la carpeta para descargar las imágenes")
+
+# guarda los argumentos recibidos en una variable
+args = vars(ap.parse_args())
+
+# crea una variable con todas las URLs para iterar mas adelante
+rows = open(args["urls"]).read().strip().split("\n")
+
+# variable que almacena el número de imágenes descargadas
+total = 0
+
+# loop las URLs
+for url in rows:
+	try:
+		# intenta descargar la imagen
+		r = requests.get(url, timeout=60)
+ 
+		# guarda la imagen en el disco
+		p = os.path.sep.join([args["output"], "{}.jpg".format(
+			str(total).zfill(8))])
+		f = open(p, "wb")
+		f.write(r.content)
+		f.close()
+ 
+		# actualiza el contador
+		print("[INFO] descargado: {}".format(p))
+		total += 1
+ 
+	# errores
+	except:
+		print("[INFO] error descargando {}...saltando".format(p))
+
+# abre cada una de las imágenes que acabamos de descargar
+for imagePath in paths.list_images(args["output"]):
+	# variable boolean que determina si la imagen se borra o no
+	delete = False
+ 
+	# intenta cargar la imagen
+	try:
+		image = cv2.imread(imagePath)
+ 
+		# si la imagen es `None` entonces no puede leerse y debe borrarse
+		if image is None:
+			delete = True
+ 
+	# si OpenCV bota error entonces la imagen esta dañada y debe borrarse
+	except:
+		print("Except")
+		delete = True
+ 
+	# si la variable `delete` es True entonces borrar la imagen
+	if delete:
+		print("[INFO] borrando {}".format(imagePath))
+		os.remove(imagePath)
